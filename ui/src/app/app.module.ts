ccd5c4f276cf697b01379a2ec3b39f5c53699445
kinozukna <ppig4016@gmail.com>
2024-05-05 16:39:22 +0300

    HQT-10 Add header to match the design

--- ui/src/app/app.module.ts
+++ ui/src/app/app.module.ts
@@ -2,13 +2,14 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { HeaderComponent } from '@shared/header/header.component';
 import { SearchModule } from '@shared/search/search.module';
 
 import { AppComponent } from './app.component';
 
 @NgModule({
     declarations: [AppComponent],
-    imports: [BrowserAnimationsModule, BrowserModule, SearchModule],
+    imports: [BrowserAnimationsModule, BrowserModule, HeaderComponent, SearchModule],
     providers: [],
     bootstrap: [AppComponent]
 })
